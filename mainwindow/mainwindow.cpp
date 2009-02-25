fc472d4a84df3af10b647aba3c377478635265ac
EdVelez.G <EdVelez.G@7f4133fc-b0cf-11dd-8748-9355f1163679>
2009-02-25 20:58:49 +0000

    changed to do 2 functionalities

--- mainwindow/mainwindow.cpp
+++ mainwindow/mainwindow.cpp
@@ -86,56 +86,82 @@ void MainWindow::definePropagationModel()
 
 void MainWindow::runSimulation()
 {
-//  textEdit->clear();
+	propagationModelTextEdit->clear();
 
-//  QString sourceFile = sourceFileEdit->text();
-//  propagationModelTextEdit->append("Hello everyone!!!");
+    QFile file("script.sh");
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
+        return;
 
-    // QFile file("script.sh");
-    // if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
-    //     return;
-	
-    // Setting the Environment for Seismic Unix
-//  QStringList env = QProcess::systemEnvironment();
-//
-//  env << "CWPROOT=/opt/SU";
-//  env << "PATH=$PATH:/opt/SU/bin";
-//
-//  process.setEnvironment(env);
-//  process2.setEnvironment(env);
+        // Stuff to write to the script
+    QTextStream out(&file);
+    out << "# /bin/sh                                                                               " << endl;
+    out << "WIDTH=450                                                                               " << endl;
+    out << "HEIGHT=450                                                                              " << endl;
+    out << "WIDTHOFF1=10                                                                            " << endl;
+    out << "WIDTHOFF2=500                                                                           " << endl;
+    out << "HEIGHTOFF1=50                                                                           " << endl;
+    out << "HEIGHTOFF2=530                                                                          " << endl;
+    out << "                                                                                        " << endl;
+    out << "n1=100 d1=5 f1=0.0 label1=\"Profundidad (km)\"                                                " << endl;
+    out << "n2=100 d2=5 f2=0.0 label2=\"Distancia (km)\"                                             " << endl;
 
-    // Setting the arguments for unif2.
+        out << qPrintable(QString("xs=%1 zs=%2 hsz=50 vsx=250 verbose=2 ")
+                                                                                                                .arg(xsSpinBox->text()).arg(zsSpinBox->text())) << endl;
 
+    out << "vsfile=\"vseis.out\" ssfile=\"sseis.out\" hsfile=\"hseis.out\"                          " << endl;
+    out << "tmax=.4 mt=5 fpeak=35 fmax=40                                                           " << endl;
+    out << "                                                                                        " << endl;
+    out << "makevel nx=$n2 nz=$n1 v000=3000 >vel.out                                                " << endl;
+    out << qPrintable(QString("unif2 < %1 n1=$n1 n2=$n2 method=spline > vel.out  ")
+                                                                                                                .arg(sourceFile)) << endl;
+    out << "unif2 < model.out n1=$n1 n2=$n2 method=spline > vel.out                                                                     " << endl;
+    out << "                                                                                        " << endl;
+    out << "ximage < vel.out wbox=$WIDTH hbox=$HEIGHT xbox=$WIDTHOFF2 title=\"Wavespeed Profile\" \\" << endl;
+    out << " n1=$n1 n2=$n2 legend=1 lheight=150 units=\"wave speed\" &                                                          " << endl;
+    out << "                                                                                        " << endl;
+    out << "sufdmod2 <vel.out nz=$n1 dz=$d1 nx=$n2 dx=$d2 verbose=1                               \\" << endl;
+    out << "    fpeak=$fpeak fmax=$fmax                                                           \\" << endl;
+    out << "    xs=$xs zs=$zs hsz=$hsz vsx=$vsx hsfile=$hsfile                                    \\" << endl;
+    out << "    vsfile=$vsfile ssfile=$ssfile verbose=$verbose                                    \\" << endl;
+    out << "    tmax=$tmax abs=1,1,1,1 mt=$mt |                                                     " << endl;
+    out << "suxmovie  clip=1.0                                                                    \\" << endl;
+    out << "    title=\"Acoustic Finite-Differencing\"                                            \\" << endl;
+    out << "    windowtitle=\"Movie\"                                                             \\" << endl;
+    out << "    label1=\"$label1\" label2=\"$label2\"                                             \\" << endl;
+    out << "    n1=$n1 d1=$d1 f1=$f1 n2=$n2 d2=$d2 f2=$f2                                         \\" << endl;
+    out << "    cmap=gray loop=1                                                                  \\" << endl;
+    out << "    -geometry ${WIDTH}x${HEIGHT}+${WIDTHOFF1}+${HEIGHTOFF1} &                           " << endl;
+    out << "                                                                                        " << endl;
+    out << "exit 0                                                                                  " << endl;
 
+    // Setting the arguments for sh.
     QStringList args;
-    args << "n1=100" << "n2=100" << "method=spline" << "label1='Depth (m)'" << "label2='Distance (m)'";
+    args << "script.sh";
+    propagationModelTextEdit->append(args.at(0));
+    process.setWorkingDirectory( QDir::current().currentPath() );
+    propagationModelTextEdit->append( QDir::current().currentPath() );
 
-    for ( QStringList::Iterator it = args.begin(); it != args.end(); ++it ) {
-        propagationModelTextEdit->append(*it);
-    }
+    // Setting the Environment for Seismic Unix
+    QStringList env = QProcess::systemEnvironment();
+    process.setEnvironment(env);
 
-//  args << "n1=100" << "n2=100" << "method=spline" << "label1='Depth (m)'" << "label2='Distance (m)'";
-//  textEdit->append(args.at(0));
-//  process2.setStandardInputFile("model.out");
-//  process2.setStandardOutputFile("vel.out");
-//  process2.setWorkingDirectory( QDir::current().currentPath() );
-//  process2.start("unif2", args);
-//  process2.waitForFinished();
-//  args.clear();
+    env << "CWPROOT=/opt/SU";
+    env << "PATH=$PATH:/opt/SU/bin";
 
-    // Setting the arguments for ximage.
-//  args << "n1=100" << "n2=100" << "d1=5" << "d2=5" << "legend=1";
-//  textEdit->append(args.at(0));
-//  process.setStandardInputFile("vel.out");
-//  process.setWorkingDirectory( QDir::current().currentPath() );
-//  textEdit->append( QDir::current().currentPath() );
-//
-//  process.start("ximage", args);
+    process.start("sh", args);
 }
 
 
 void MainWindow::runXimage()
 {
+  	// Setting the Environment for Seismic Unix
+	QStringList env = QProcess::systemEnvironment();
+ 	env << "CWPROOT=/opt/SU";
+ 	env << "PATH=$PATH:/opt/SU/bin";
+ 	process.setEnvironment(env);
+	process2.setEnvironment(env);
+
+	
     velocityModelTextEdit->clear();
 
     QString n1 = n1SpinBox->text();
@@ -144,64 +170,44 @@ void MainWindow::runXimage()
     QString d2 = d2SpinBox->text();
 
 //  QString sourceFile = sourceFileEdit->text();
+	// Setting arguments for unif2
     velocityModelTextEdit->append("unif2");
-
     QStringList args;
-    args << "n1=" + n1 
-         << "n2=" + n2
-        ;
+    args 	<< "n1=" + n1 
+	       	<< "n2=" + n2
+	        << "method=spline"
+	        ;
 
     for ( QStringList::Iterator it = args.begin(); it != args.end(); ++it ) {
         velocityModelTextEdit->append(*it);
     }
 
+	// Process unif2.
+	process2.setStandardInputFile("model.out");
+	process2.setStandardOutputFile("vel.out");
+	process2.setWorkingDirectory( QDir::current().currentPath() );
+	process2.start("unif2", args);
+	process2.waitForFinished();
 
     args.clear();
-
+	
+	// Setting arguments for ximage
     velocityModelTextEdit->append("ximage");
-
-    args << "n1=" + n1 
-         << "n2=" + n2
-         << "d1=" + d1
-         << "d2=" + d2
-        ;
+    args	<< "n1=" + n1 
+         	<< "n2=" + n2
+         	<< "d1=" + d1
+         	<< "d2=" + d2
+         	<< "legend=1"
+        	;
 
     for ( QStringList::Iterator it = args.begin(); it != args.end(); ++it ) {
         velocityModelTextEdit->append(*it);
     }
 
-    // QFile file("script.sh");
-    // if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
-    //     return;
-	
-    // Setting the Environment for Seismic Unix
-//  QStringList env = QProcess::systemEnvironment();
-//
-//  env << "CWPROOT=/opt/SU";
-//  env << "PATH=$PATH:/opt/SU/bin";
-//
-//  process.setEnvironment(env);
-//  process2.setEnvironment(env);
-//
-//  // Setting the arguments for unif2.
-//  QStringList args;
-//  args << "n1=100" << "n2=100" << "method=spline" << "label1='Depth (m)'" << "label2='Distance (m)'";
-//  textEdit->append(args.at(0));
-//  process2.setStandardInputFile("model.out");
-//  process2.setStandardOutputFile("vel.out");
-//  process2.setWorkingDirectory( QDir::current().currentPath() );
-//  process2.start("unif2", args);
-//  process2.waitForFinished();
-//  args.clear();
-//
-//  // Setting the arguments for ximage.
-//  args << "n1=100" << "n2=100" << "d1=5" << "d2=5" << "legend=1";
-//  textEdit->append(args.at(0));
-//  process.setStandardInputFile("vel.out");
-//  process.setWorkingDirectory( QDir::current().currentPath() );
-//  textEdit->append( QDir::current().currentPath() );
-//
-//  process.start("ximage", args);
+	process.setStandardInputFile("vel.out");
+	process.setWorkingDirectory( QDir::current().currentPath() );
+	process.start("ximage", args);
+
 }
 
 bool MainWindow::save()
